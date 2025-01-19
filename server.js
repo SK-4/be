1f70a04ffc6efdbd5e4ae92e89ca3fd9c51731c6
SK-4 <sohamk2002@gmail.com>
2025-01-19 16:02:10 +0530

    commit-api

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// API Endpoint
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello World" });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
